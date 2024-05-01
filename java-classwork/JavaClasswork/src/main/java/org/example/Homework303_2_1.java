466e090178bc9b3e9d3c1f7033069a11049cceee
Crystal Ramos <crystalramos.cmr@gmail.com>
2024-05-01 18:42:25 -0400

    Homework303_2_1

---
+++ java-classwork/JavaClasswork/src/main/java/org/example/Homework303_2_1.java
@@ -0,0 +1,65 @@
+package org.example;
+
+public class Homework303_2_1 {
+
+    public static void main(String[] args) {
+
+        //Write a program that declares two integer variables, assigns an integer to each, and adds them together.
+        //Assign the sum to a variable. Print out the result.
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        System.out.println("The sume of " + sum);
+
+
+        //Write a program that declares two double variables, assigns a number to each, and adds them together.
+        //Assign the sum to a variable. Print out the result.
+        double d1 = 10.5d;
+        double d2 = 20.5d;
+
+        System.out.println(d1 + d2);
+
+        //Write a program that declares an integer variable and a double variable, assigns numbers to each, and adds them together. Assign the sum to a variable.
+        //Print out the result. What variable type must the sum be?
+
+        int e1 = 10;
+        double e2 = 20.5d;
+        double sum3 = e1 + e2;
+
+        System.out.println(sum3);
+
+        //Write a program that declares two integer variables, assigns an integer to each, and divides the larger number by the smaller number. Assign the result to a variable.
+        //Print out the result. Now change the larger number to a decimal. What happens? What corrections are needed?
+        int f1 = 80;
+        int f2 = 50;
+        double sum4 = (double)f1 / (double)f2;
+
+        System.out.println(sum4);
+
+        //Write a program that declares two double variables, assigns a number to each, and divides the larger by the smaller number. Assign the result to a variable.
+        //Print out the result. Now, cast the result to an integer. Print out the result again.
+        double g1 = 50.5d;
+        double g2 = 25.5d;
+        int sum5 = (int)g1 / (int)g2;
+
+        System.out.println(sum5);
+
+
+        //Write a program that declares two integer variables, x and y, and assigns the number 5 to x and the number 6 to y.
+        //Declare a variable q and assign y/x to it and print q. Now, cast y to a double and assign it to q. Print q again.
+        int x = 85;
+        int y = 6;
+        double q = y/x;
+
+        System.out.println(q);
+
+       q = (double) y / x;
+
+        System.out.println(q);
+
+    }
+    //Write a program that declares a named constant and uses it in a calculation. Print the result.
+    static final int constantName = 10;
+}
+ 
