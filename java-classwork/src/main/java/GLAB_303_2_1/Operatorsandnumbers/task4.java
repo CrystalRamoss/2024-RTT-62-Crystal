2947d5974d4249d91e51f765339f6bbf330256e4
Crystal Ramos <crystalramos.cmr@gmail.com>
2024-05-02 18:50:13 -0400

    HW Update

--- java-classwork/src/main/java/GLAB_303_2_1/Operatorsandnumbers/task4.java
+++ java-classwork/src/main/java/GLAB_303_2_1/Operatorsandnumbers/task4.java
@@ -1,2 +1,16 @@
-package GLAB_303_2_1.Operatorsandnumbers;public class task4 {
+package GLAB_303_2_1.Operatorsandnumbers;
+
+public class task4 {
+    public static void main(String[] args){
+
+
+        int x = 7;
+        int y = 17;
+        int z = x | y;
+
+        System.out.println(z);
+
+
+
+    }
 }
