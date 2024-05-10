e9f4efa09c5f9793341d167ff3584f8b3922fbc6
crystalogy <crystalramos.cmr@gmail.com>
2024-05-10 10:03:54 -0400

    hw

--- java-classwork/src/main/java/org/example/PA_303_9_1/Java_InheritanceII/Arithmetic.java
+++ java-classwork/src/main/java/org/example/PA_303_9_1/Java_InheritanceII/Arithmetic.java
@@ -6,15 +6,10 @@ public class Arithmetic {
     private int input2;
     private int sum;
 
-    public Arithmetic(int input1, int input2, int sum){
+    public void adder (int input1, int input2){
         this.input1 = input1;
         this.input2 = input2;
-        this.sum = sum;
-    }
 
-    public int add (int x, int y) {
-        sum = x + y;
-        return sum;
     }
 
 }
