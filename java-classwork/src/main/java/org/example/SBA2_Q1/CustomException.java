96a75d52b2bbb518d2c6d3a64068cab632691b4c
crystalogy <crystalramos.cmr@gmail.com>
2024-05-20 15:46:09 -0400

    classwork/ practice for SBA

---
+++ java-classwork/src/main/java/org/example/SBA2_Q1/CustomException.java
@@ -0,0 +1,9 @@
+package org.example.SBA2_Q1;
+
+public class CustomException extends Exception{
+    public CustomException(String message) {
+        // the super keyword passes the message to the constructor
+        // of the parent
+        super(message);
+    }
+}
---
+++ java-classwork/src/main/java/org/example/SBA2_Q2/Sport.java
@@ -0,0 +1,10 @@
+package org.example.SBA2_Q2;
+
+public interface Sport {
+    // this will be given to us already in the SBA
+
+    void calculateAvgAge(int[] age);
+
+    void retirePlayer(int id);
+
+}
