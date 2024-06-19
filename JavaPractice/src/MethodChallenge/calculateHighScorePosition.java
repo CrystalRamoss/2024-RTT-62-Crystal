e4ad8832cf4ff21f4f401e064b1174913223a213
crystalogy <crystalramos.cmr@gmail.com>
2024-06-19 15:24:59 -0400

    no message

--- JavaPractice/src/MethodChallenge/calculateHighScorePosition.java
+++ JavaPractice/src/MethodChallenge/calculateHighScorePosition.java
@@ -1,2 +1,18 @@
-package MethodChallenge;public class calculateHighScorePosition {
+package MethodChallenge;
+
+public class calculateHighScorePosition {
+
+    public static int HighScorePosition(int score) {
+
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500 && score < 1000) {
+            return 2;
+        } else if (score >= 100 && score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+
+    }
 }
