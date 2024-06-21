3b06f960a34f75b7dd4c984c0d0af0c38d1fb252
crystalogy <crystalramos.cmr@gmail.com>
2024-06-21 09:27:18 -0400

    no message

--- JavaPractice/src/MethodOverloadingChallenge/Main.java
+++ JavaPractice/src/MethodOverloadingChallenge/Main.java
@@ -1,9 +1,7 @@
-package MethodChallenge;
+package MethodOverloadingChallenge;
 
 public class Main {
     public static void main(String[] args) {
-        int playerPosition = calculateHighScorePosition.HighScorePosition(25);
-        String output = displayHighScorePosition.HighScorePosition("Crystal", playerPosition);
-        System.out.println(output);
+        System.out.println(inchesToCentimeters.convertToCentimeters(5, 10));
     }
 }
