c2aa9322d4919ac0838f0f1906cf32225e066a24
crystalogy <crystalramos.cmr@gmail.com>
2024-06-21 09:26:34 -0400

    no message

--- JavaPractice/src/SecondsAndMinutesChallenge/SecondsAndMinutes.java
+++ JavaPractice/src/SecondsAndMinutesChallenge/SecondsAndMinutes.java
@@ -1,2 +1,29 @@
-package SecondsAndMinutesChallenge;public class SecondsAndMinutes {
+package SecondsAndMinutesChallenge;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class SecondsAndMinutes {
+
+    public static String getDurationString(int seconds){
+        if ( seconds >= 0) {
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            return getDurationString(seconds, minutes);
+        } else {
+            return "Invalid Duration";
+        }
+    }
+
+    public static String getDurationString(int seconds, int minutes){
+        if (minutes >= 0 && seconds <= 59) {
+            minutes = minutes + (seconds / 60);
+            int hours = minutes / 60;
+            minutes = minutes % 60;
+            seconds = seconds % 60;
+            return hours + "h" + minutes + "m" + seconds + "s";
+        } else {
+            return "Invalid Duration";
+        }
+    }
+
 }
