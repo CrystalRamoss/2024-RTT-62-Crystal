d217e99940a46b5ec8cc255733b240360eee9657
crystalogy <crystalramos.cmr@gmail.com>
2024-06-21 09:26:10 -0400

    no message

--- JavaPractice/src/Test/text.java
+++ JavaPractice/src/Test/text.java
@@ -1,2 +1,7 @@
-package Test;public class text {
+package Test;
+
+public class text {
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+    }
 }
