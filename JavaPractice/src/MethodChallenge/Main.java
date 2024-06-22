bcfc6aaeb7e95b8ad14fc116330663a212d5be09
crystalogy <crystalramos.cmr@gmail.com>
2024-06-22 17:13:33 -0400

    no message

---
+++ JavaPractice/src/MethodChallenge/Main.java
@@ -0,0 +1,9 @@
+package MethodChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        int playerPosition = calculateHighScorePosition.HighScorePosition(25);
+        String output = displayHighScorePosition.HighScorePosition("Crystal", playerPosition);
+        System.out.println(output);
+    }
+}
